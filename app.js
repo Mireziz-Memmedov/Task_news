02f66de3304d90c7e04c09c57daaa0644a3c1546
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-11-01 16:17:04 +0000

    commit

--- app.js
+++ app.js
@@ -69,4 +69,19 @@ $(document).ready(function () {
 
         mynews();
     });
+
+
+    $(document).on("keypress", function (e) {
+        if (e.which == 13) {
+            let url = ``;
+            async function mynews() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                console.log(data);
+            }
+            mynews();
+        }
+    });
+
+
 });
